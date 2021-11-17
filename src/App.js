d3a0f4b9b00c764b6e724d99ba7335b19a51d690
Caedan <caedanlavender@gmail.com>
2021-11-17 22:48:26 +1300

    Started navigation section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Portfolio</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header>
+				<div className='item'>
+					<h1>Caedan</h1>
+				</div>
+				<div className='item'>
+					=
+				</div>
+			</header>
+			<h1>Portfolio</h1>
+		</div>
+	);
 }
 
 export default App;
