ee07a0b88bac66626caa3fd179e7fa8491a9c561
Caedan <caedanlavender@gmail.com>
2021-12-27 17:19:47 +1300

    Added array delimiting function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,7 @@
 export const toSentenceCase = (word) => {
    return word[0].toUpperCase() + word.substr(1, word.length - 1);
 }
+
+export const delimitArray = (array, delimeter) => {
+   return array.flatMap((each, i) => i ? [delimeter, each] : [each]);
+}
